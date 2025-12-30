babe2363dfdbb6b587520fef90326b311a2c3eba
Jonathan French <jaypeeff@gmail.com>
2025-12-30 14:21:24 +0000

    Initial Commit

---
+++ Vibe-Chess/Vibe_ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vibe_ChessApp.swift
+//  Vibe-Chess
+//
+//  Created by Jonathan French on 30.12.25.
+//
+
+import SwiftUI
+
+@main
+struct Vibe_ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
